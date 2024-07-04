aa6f43a690669d864ffd9bce562663f1f2dbad5b
luizleal <luizandreleal100@gmail.com>
2024-07-04 18:53:31 -0300

    text color changed again

--- themes/index.js
+++ themes/index.js
@@ -15,7 +15,7 @@ export const themes = {
   transparent: {
     title_color: "3ddb86",
     icon_color: "31b56e",
-    text_color: "417e87",
+    text_color: "386d76",
     bg_color: "ffffff00",
   },
   shadow_red: {
